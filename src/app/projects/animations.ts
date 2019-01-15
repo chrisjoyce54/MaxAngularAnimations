efc5dfaed5eaf73a16c516d2bae9fce762c9e011
Chris Joyce <CJoyce@jhacorp.com>
2019-01-15 12:32:36 -0600

    SLide Out lecture 96

--- src/app/projects/animations.ts
+++ src/app/projects/animations.ts
@@ -30,3 +30,22 @@ export const markedTrigger = trigger('markedState', [
   ]),
   // transition('marked => default', animate('300ms ease-out')),
 ]);
+
+export const itemStatetrigger = trigger('itemState', [
+  transition(':enter', [
+    style({
+      opacity: 0,
+      transform: 'translateX(-100%)'
+    }),
+    animate('500ms ease-out', style({
+      opacity: 1,
+      transform: 'translateX(0)'
+    }))
+  ]),
+  transition(':leave', [
+    animate('1500ms ease-in', style({
+      opacity: 0,
+      transform: 'translateX(100%)'
+    }))
+  ])
+]);
